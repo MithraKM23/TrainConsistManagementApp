0fb2f9a25b54e6c7dd6f3a0e273083078efcf366
MithraKM23 <mithrakm2005@gmail.com>
2026-03-10 14:25:06 +0530

    Allow dynamic insertion and removal of passenger bogies using ArrayList.

--- src/com/TrainConsistManagementApp/main/Main.java
+++ src/com/TrainConsistManagementApp/main/Main.java
@@ -1,6 +1,6 @@
 /*
  * @author Developer 
- * @version 1.0
+ * @version 2.0
  */
 
 package com.TrainConsistManagementApp.main;
@@ -13,18 +13,37 @@ public class Main {
 		
 		//Display welcome banner
 		System.out.println("====================================");
-		System.out.println("=== Train Consist Management App ===");
+		System.out.println("=== Add Pasanger bogies to train ===");
 		System.out.println("====================================");
 		
 		//Create a dynamic list to store train bogies
 		List<String> trainConsist = new ArrayList<>();
 		
-		//Display initial consist information
+		//Adding train bogies to the list
 		System.out.println("\n");
-		System.out.println("Train Initialised successfully...");
-		System.out.println("Initial bogie count: "+trainConsist.size());
-		System.out.println("Current train Consist: "+trainConsist);
+		System.out.println("After Adding Bogies: ");
+		trainConsist.add("Sleeper");
+		trainConsist.add("AC Chair");
+		trainConsist.add("First Class");
+		System.out.println("Passenger Bogies : "+trainConsist);
+		
+		//Removing AC chair from the list
 		System.out.println("\n");
-		System.out.println("System ready for operations...");
+		System.out.println("After removing 'AC Chair' :");
+		trainConsist.remove(1);
+		System.out.println("Passenger Bogies : "+trainConsist);
+		
+		//Checking if sleeper exists in the bogie train
+		System.out.println("\n");
+		System.out.println("Checking if 'Sleeper' exists:");
+		System.out.println("Contains Sleeper? : "+trainConsist.contains("Sleeper"));
+		
+		System.out.println("\n");
+		System.out.println("Final Train Passenger Consist:");
+		System.out.println(trainConsist);
+		
+		System.out.println("\n");
+		System.out.println("UC2 operations completed successfully");
+		
 	}
 }
