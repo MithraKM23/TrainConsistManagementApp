fd9c42e1171b7e9c49dbfb96a944d6d154192553
MithraKM23 <mithrakm2005@gmail.com>
2026-03-12 14:07:26 +0530

    Sort passenger bogie capacities using bubble sort

--- src/com/TrainConsistManagementApp/main/Main.java
+++ src/com/TrainConsistManagementApp/main/Main.java
@@ -1,66 +1,49 @@
 /*
  * @author Developer 
- * @version 15.0
+ * @version 16.0
  */
 
 package com.TrainConsistManagementApp.main;
 
 public class Main {
 	
-	//Goods Bogie model
-	static class GoodsBogie{
-		String shape;
-		String cargo;
-		public GoodsBogie(String shape) {
-			
-			this.shape = shape;
-		}
-		
-		//Assign cargo with safety validation
-		public void assignCargo(String cargo) {
-			try {
-				
-				//Rule: Rectangular bogie cannot carry petroleum
-				if(shape.equalsIgnoreCase("Rectangular") && cargo.equalsIgnoreCase("Petroleum")) {
-					throw new CargoSafetyException("Unsafe assignment!");
-				}
-				this.cargo=cargo;
-				System.out.println("Cargo assigned sucessfully -> "+cargo);
-			}
-			catch(CargoSafetyException e) {
-				System.out.println("Error: "+e.getMessage());
-			}
-			finally {
-				System.out.println("Cargo validation completed for "+shape+" bogie");
-			}
-		}
-	}
-	
-	//Custom Runtime Exception
-	static class CargoSafetyException extends RuntimeException{
-
-		public CargoSafetyException(String msg) {
-			super(msg);
-		}	
-	}
-
 	public static void main(String[] args) {
 
 		//Display welcome banner
-		System.out.println("====================================");
-		System.out.println("=== UC15 - Safe Cargo Assignment ===");
-		System.out.println("====================================\n");
+		System.out.println("===============================================");
+		System.out.println("=== UC16 - Manual Sorting using Bubble Sort ===");
+		System.out.println("===============================================\n");
 		
-		//Creating a bogie and assigning a cargo
-		GoodsBogie bogie1=new GoodsBogie("Cylindrical");
-		bogie1.assignCargo("Petroleum");
+		int[] capacities = {72,56,24,70,60};
+		
+		System.out.println("Original Capacities: ");
+		for(int c : capacities) {
+			System.out.print(c+" ");
+		}
+		
+		for(int i=0;i<capacities.length-1;i++) {
+			boolean swapped=true;
+			for(int j=0;j<capacities.length-i-1;j++) {
+				if(capacities[j]>capacities[j+1]) {
+					int temp=capacities[j];
+					capacities[j]=capacities[j+1];
+					capacities[j+1]=temp;
+					swapped=false;
+				}
+			}
+			if(swapped) {
+				break;
+			}
+		}
 		
 		System.out.println("\n");
-		GoodsBogie bogie2=new GoodsBogie("Rectangular");
-		bogie2.assignCargo("Petroleum");
+		System.out.println("Sorted Capacities (Ascending): ");
+		for(int c : capacities) {
+			System.out.print(c+" ");
+		}
 
 		System.out.println("\n");
-		System.out.println("UC15 runtime handling completed...");
+		System.out.println("UC16 sorting completed...");
 
 	}
 }
