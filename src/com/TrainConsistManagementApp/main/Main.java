3557e24bfd949fc9031afcc2a57b06820f558dee
MithraKM23 <mithrakm2005@gmail.com>
2026-03-16 08:32:49 +0530

    Find a bogie ID efficiently using binary search on sorted data.

--- src/com/TrainConsistManagementApp/main/Main.java
+++ src/com/TrainConsistManagementApp/main/Main.java
@@ -1,6 +1,6 @@
 /*
  * @author Developer 
- * @version 18.0
+ * @version 19.0
  */
 
 package com.TrainConsistManagementApp.main;
@@ -13,38 +13,56 @@ public class Main {
 
 		//Display welcome banner
 		System.out.println("=========================================");
-		System.out.println("=== UC18 - Linear Search for Bogie ID ===");
+		System.out.println("=== UC19 - Binary Search for Bogie ID ===");
 		System.out.println("=========================================\n");
 		
 		//Creating array of bogie IDs
 		String[] bogiesIds= {"BG101", "BG205", "BG309", "BG412", "BG550"};
 		
-		String searchId="BG309";
+		//Ensuring data is sorted before binary search
+		Arrays.sort(bogiesIds);
 		
+		//Search key
+		String key="BG309";
+		
+		//Display available bogies
 		System.out.println("Available Bogie IDs:");
 		for(String id:bogiesIds) {
 			System.out.println(id);
 		}
 		
-		//Using linear search to find if the bogie is present or not
-		boolean found=false;
-		 for(String id:bogiesIds) {
-			 if(id==searchId) {
-				 found=true;
-			 }
-		 }
+		boolean result=false;
+		int low=0;
+		int high=bogiesIds.length-1;
 		
-		if(found) {
-			System.out.println("Bogie "+searchId+" found in train consist.");
+		//Binary search logic
+		while(low<=high) {
+			int mid=(low+high)/2;
+			int output=key.compareTo(bogiesIds[mid]);
+			if(output==0) {
+				result=true;
+				break;
+			}
+			else if(output < 0) {
+				high=mid-1;
+			}
+			else {
+				low=mid+1;
+			}
+			result=false;
+		}
+		
+		if(result) {
+			System.out.println("Bogie "+key+" found in train consist.");
 		}
 		else {
-			System.out.println("Bogie "+searchId+" not found in train consist.");
+			System.out.println("Bogie "+key+" not found in train consist.");
 
 		}
 		
 		
 		System.out.println("\n");
-		System.out.println("UC18 search completed...");
+		System.out.println("UC19 search completed...");
 
 	}
 }
